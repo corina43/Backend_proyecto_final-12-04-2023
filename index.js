1bb1ec2618e5c30e1e98ac440830ec1021eca68f
corina <ciorpaccorina@gmail.com>
2023-04-12 22:53:14 +0200

    feat:connecting database with server

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require ("express")
-const app = express()
-const PORT = 3001
-app.use(express.json)
-app.listen(PORT, () => {
-    console.log("hola estoy conectado");
+const express = require('express');
+// const cors = require('cors')
+const app = express();
+
+const db = require('./db.js');
+require('dotenv').config()
+const router = require('./router')
+
+const PORT = process.env.PORT;
+
+// app.use(cors())
+app.use(express.json());
+app.use(router)
+
+db.then(() => {
+    app.listen(PORT, () => console.log("Server on port " + PORT));
 })
+.catch((error) => console.log(error.message));
