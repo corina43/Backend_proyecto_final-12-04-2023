737ad643a2993627b07bd099926e6d7726d9e0c6
corina <ciorpaccorina@gmail.com>
2023-04-13 12:26:30 +0200

    feat:created migrations prestamos

---
+++ migrations/20230413095803-create-prestamos.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up:  async(queryInterface, Sequelize) =>{
+   await queryInterface.createTable('Prestamos',{
+    id:{
+      allowNull:false,
+      autoIncrement:true,
+      primaryKey:true,
+      type:Sequelize.INTEGER,
+
+    },
+    id_usuario: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references:{
+        model:'Usuarios',
+        key:'id',
+      },
+    },
+    id_producto: {
+      type:Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Productos',
+        key: 'id',
+      },
+    },
+    fecha_inicio: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    fecha_fin: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    puntuacion: {
+      type:Sequelize.INTEGER,
+    },
+    comentario_producto: {
+      type: Sequelize.TEXT,
+    },
+    createdAt:{
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updateAt:{
+      allowNull:false,
+      type:Sequelize.DATE,
+    },
+   });
+  },
+
+   down: async(queryInterface, Sequelize) => {
+  await queryInterface.dropTable('Prestamos')
+  },
+};
